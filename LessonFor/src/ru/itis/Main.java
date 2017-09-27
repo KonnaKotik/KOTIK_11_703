312a3913ef5aa9599bc23ec57bcdf6f8be018ca8
Арина Котик <KonnaKotik>
2017-09-27 15:58:11 +0500

    add for

--- LessonFor/src/ru/itis/Main.java
+++ LessonFor/src/ru/itis/Main.java
@@ -9,10 +9,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        int i = a;
-        while(i <= b){
+//        int i = a;
+//        while(i <= b){
+//            System.out.print(i + " ");
+//            i++;
+//        }
+        for (int i =a; i <= b; i++){
             System.out.print(i + " ");
-            i++;
         }
     }
 }
