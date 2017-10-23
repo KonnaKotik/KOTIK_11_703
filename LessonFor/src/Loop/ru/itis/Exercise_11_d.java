ef8791feb6fd50c7dfb79a452db98265e71492f8
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-23 23:50:29 +0300

    Update Exercise_11_d.java

--- LessonFor/src/Loop/ru/itis/Exercise_11_d.java
+++ LessonFor/src/Loop/ru/itis/Exercise_11_d.java
@@ -1,4 +1,4 @@
-package ru.itis;
+package ru.itis;//-
 
 import java.util.Scanner;
 
@@ -16,7 +16,8 @@ public class Exercise_11_d {
 
         while (i <= n) {
             s *= t;
-            t *= Math.sin(i) / Math.cos(i);
+            t *= Math.sin(i) / Math.cos(i);//как-то вы странно формируете тело цикла. При i = n получаем sin(n) и cos(n),
+            //а i становится равным n+1 и подсчитанное t в S не попадает.
             i++;
         }
         System.out.println(s);
